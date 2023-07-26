680c05f7fa570c82a7d0c2896732093e3e9c796a
DanielSinTY <tysin@connect.ust.hk>
2023-07-26 11:20:22 +0800

    Added alerts and user settings for alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 import ECommerce from './pages/Dashboard/ECommerce';
 import SignIn from './pages/Authentication/SignIn';
@@ -20,6 +21,8 @@ function App() {
     <Loader />
   ) : (
     <>
+    <Toaster position='top-right' reverseOrder={false} containerClassName='overflow-auto'/>
+  
       <Routes>
         <Route path="/auth/signin" element={<SignIn />} />
         <Route path="/auth/signup" element={<SignUp />} />
