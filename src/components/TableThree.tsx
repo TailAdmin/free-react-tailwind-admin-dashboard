3f07acf440d7f62bbe8cc69bc2bcfd19df4a6de6
SunnyFarmDay <SunnyFarmDay@gmail.com>
2023-07-28 17:58:49 +0800

    implemented colFilter query generatation

--- src/components/TableThree.tsx
+++ src/components/TableThree.tsx
@@ -5,19 +5,19 @@ const TableThree = () => {
   const [data, setData] = useState({ '': { '': '' } });
   const [filteredData, setFilteredData] = useState({ '': { '': '' } });
   const [colFilterShow, setColFilterShow] = useState(false);
-  const [colFilter, setColFilter] = useState(['']);
+  const [colFilter, setColFilter] = useState({});
 
   const [filterOperators, setFilterOperators] = useState({});
   const [filterValues, setFilterValues] = useState({});
   const [showFilterBox, setShowFilterBox] = useState(false);
 
-  async function fetchData(param: string = '') {
-    const response = await fetch(`${API_URL}/data?${param}`);
+  async function fetchData(route: string='data?', param: string = '') {
+    const response = await fetch(`${API_URL}/${route}${param}`);
     if (!response.ok) {
-      return null
+      return null;
     }
     const jsonData = await response.json();
-    console.log('called', `${API_URL}/data?${param}`);
+    console.log('called', `${API_URL}/${route}${param}`);
     return jsonData;
   }
 
@@ -26,16 +26,23 @@ const TableThree = () => {
       const apiData = await fetchData();
       setData(apiData);
       setFilteredData(apiData);
+      const availableCols = await fetchData('available_cols','')
+      let colFilters = {};
+      (availableCols).map((col: string) => {
+        colFilters = { ...colFilters, [col]: true };
+      });
+      setColFilter(colFilters)
+
+    
+      let filters = {};
+      Object.keys(Object.values(apiData)[0]).map((col) => {
+        filters = { ...filters, [col]: '' };
+      });
+      setFilterValues(filters);
     }
     initData();
-    setColFilter(Object.keys(Object.values(data)[0]));
-    let filters: {};
-    Object.keys(Object.values(data)[0]).map((col) => {
-      filters = { ...filters, [col]: '' };
-    });
+    
 
-    setFilterValues(filters);
-    console.log(filters);
   }, []);
 
   // column filter
@@ -44,7 +51,6 @@ const TableThree = () => {
   }
 
   // show filter box
-
   function toggleShowFilterBox(event: any) {
     event.preventDefault();
     setShowFilterBox(!showFilterBox);
@@ -54,115 +60,63 @@ const TableThree = () => {
   function handleSelectedOption(name: string, value: string) {
     const updatedOperators = { ...filterOperators, [name]: value };
     setFilterOperators(updatedOperators);
-    handleFilters(updatedOperators, filterValues);
+    handleFilters(updatedOperators, filterValues, colFilter);
   }
   function handleFilterValue(event, col) {
     const values = { ...filterValues };
     values[col] = event.target.value;
     setFilterValues(values);
-    handleFilters(filterOperators, values);
+    handleFilters(filterOperators, values, colFilter);
   }
-
-  function handleFilters(filterOperators, filterValues) {
+  // handle filters, cols filter
+  function handleFilters(filterOperators, filterValues, colFilters) {
     let bondidQuery: string = '';
     let filterQuery: string = '';
+    let colFilterQuery: string = '';
     Object.entries(filterOperators).map(([col, operator]) => {
       if (filterValues[col]) {
         if (col === 'BondID') {
-          bondidQuery = "&bond_ids="+filterValues[col]
-        }
-        else if (isNaN(Object.values(data)[0][col])) {
+          bondidQuery = '&bond_ids=' + filterValues[col];
+        } else if (isNaN(Object.values(data)[0][col])) {
           let values: string = '';
-          filterValues[col].split(',').map((value)=> {
-            values=values+"'"+value.trim()+"',"
-          })
-          values = values.slice(0, -1)
-          filterQuery = filterQuery+col+operator+'('+values+'),'
-        }
-        else {
-          filterQuery = filterQuery+col+operator+filterValues[col]+','
-
+          filterValues[col].split(',').map((value) => {
+            values = values + "'" + value.trim() + "',";
+          });
+          values = values.slice(0, -1);
+          filterQuery = filterQuery + col + operator + '(' + values + '),';
+        } else {
+          filterQuery = filterQuery + col + operator + filterValues[col] + ',';
         }
       }
     });
+
+    Object.keys(colFilter).map((col) => {
+      if (colFilter[col]) {
+        colFilterQuery = colFilterQuery + col + ','
+      }
+    })
+    colFilterQuery="&cols="+colFilterQuery.slice(0,-1)
+
     let param: string = '';
     if (filterQuery) {
-      param = 'filters='+filterQuery.slice(0, -1)
+      param = 'filters=' + filterQuery.slice(0, -1);
     }
-    param = param + bondidQuery
+    param = param + bondidQuery;
     async function getFilteredData() {
-      const filtered = await fetchData(param)
-      setFilteredData(filtered)
+      const filtered = await fetchData('data?',param);
+      setFilteredData(filtered);
     }
-    getFilteredData()
 
-    // console.log()
-    // let reset = true;
-    // Object.entries(filterOperators).map(([col, operator]) => {
-    //   if (filterValues[col]) {
-    //     let filter = filterValues[col]
-    //     reset = false
-    //     console.log(filterValues[col])
-    //     let filtered;
-    //     let result = {};
-    //     if (col === 'BondID') {
-    //       switch (operator) {
-    //         case '=':
-    //           filter.split(',').map((thefilter: string)=>(
 
-    //           ))
-    //           filtered = Object.entries(data).filter(([bondid, _]) =>
-    //             bondid.match(filter) ? true : false
-    //           );
-    //           break;
-    //         case '!=':
-    //           filtered = Object.entries(data).filter(([bondid, _]) =>
-    //             bondid.match(filter) ? false : true
-    //           );
-    //           break;
-    //         case '>':
-    //           break;
-    //         case '<':
-    //           break;
-    //         case '>=':
-    //           break;
-    //         case '<=':
-    //           break;
-    //       }
-
-    //     } else if (isNaN(data[col])) {
-    //       switch (operator) {
-    //         case '=':
-    //           const updatedData = { ...filteredData };
-
-    //         case '!=':
-    //           return item[colName] != value;
-    //       }
-    //     } else {
-    //       switch (operator) {
-    //         case '=':
-    //           return item[colName] == value;
-    //         case '!=':
-    //           return item[colName] != value;
-    //         case '>':
-    //           return item[colName] > value;
-    //         case '<':
-    //           return item[colName] < value;
-    //         case '>=':
-    //           return item[colName] >= value;
-    //         case '<=':
-    //           return item[colName] <= value;
-    //       }
-    //     }
-    //     filtered.map((entry)=> (
-    //     result[entry[0]] = entry[1]))
-    //     console.log(result)
-    //     setFilteredData(result)
-    //   }
-    // });
-    // if (reset) {
-    //   setFilteredData(data)
-    // }
+    getFilteredData();
+  }
+  function toggleColFilter(col: string) {
+    if (col === 'BondID') {
+      return
+    }
+    const updateColFilter = { ...colFilter };
+    updateColFilter[col] = !colFilter[col];
+    setColFilter(updateColFilter);
   }
   return (
     <div className="relative rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
@@ -195,19 +149,19 @@ const TableThree = () => {
             (colFilterShow ? '' : ' hidden')
           }
         >
-          {Object.keys(Object.values(data)[0]).map((key) => {
+          {Object.keys(colFilter).map((col) => {
             return (
               <div className="bg-gray px-3 py-1">
-                <p>
+                <div onClick={() => {toggleColFilter(col)}}>
                   <input
                     type="checkbox"
-                    name={'check_' + key}
-                    id={'check_' + key}
+                    name={'check_' + col}
+                    id={'check_' + col}
                     className="mr-2"
-                    checked
+                    checked={colFilter[col]}
                   />
-                  {key}
-                </p>
+                  <label htmlFor={'check_' + col} className='pointer-events-none'>{col}</label>
+                </div>
               </div>
             );
           })}
@@ -220,8 +174,8 @@ const TableThree = () => {
         <table className="w-full">
           <thead className="">
             <tr className="text-left dark:bg-meta-4">
-              <th className="sticky top-0 left-0 z-99">
-                <div className=" flex flex-col min-w-[13rem] bg-gray-2 py-4 font-medium text-black dark:bg-meta-4 dark:text-white xl:pl-11">
+              <th className="sticky top-0 left-0 z-50">
+                <div className=" flex min-w-[13rem] flex-col bg-gray-2 py-4 font-medium text-black dark:bg-meta-4 dark:text-white xl:pl-11">
                   <h4 className="block px-4">BondID</h4>
                   <div
                     className={
@@ -248,7 +202,7 @@ const TableThree = () => {
                       type="text"
                       name="filter_value"
                       id=""
-                      className="border-2 border-gray px-1 w-full"
+                      className="w-full border-2 border-gray px-1"
                       value={filterValues['BondID']}
                       onChange={(event) => handleFilterValue(event, 'BondID')}
                     />
@@ -296,7 +250,7 @@ const TableThree = () => {
                           type="text"
                           name="filter_value"
                           id=""
-                          className="border-2 border-gray px-1 w-full"
+                          className="w-full border-2 border-gray px-1"
                           value={filterValues[col]}
                           onChange={(event) => handleFilterValue(event, col)}
                         />
