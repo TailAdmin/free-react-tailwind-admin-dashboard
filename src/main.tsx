5ed299829b19b48e4074b11c2776ef637a8d9d2c
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2024-11-15 13:21:40 -0600

    fix import jsvectormap package

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './css/style.css';
 import './css/satoshi.css';
-import 'jsvectormap/dist/css/jsvectormap.css';
+// import 'jsvectormap/dist/css/jsvectormap.css';
+import jsVectorMap from 'jsvectormap'
+import 'jsvectormap/dist/maps/world.js'
 import 'flatpickr/dist/flatpickr.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
 );
