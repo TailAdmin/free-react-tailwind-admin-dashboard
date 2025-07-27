7da27f5ddadeaed2212ddc861dd728450372d324
DAVID ADRIAN LUCANO NIEVES <133066087+David42024@users.noreply.github.com>
2025-07-27 10:10:44 -0500

    Update FormElements.tsx
    pageTitle fixed

--- src/pages/Forms/FormElements.tsx
+++ src/pages/Forms/FormElements.tsx
@@ -18,7 +18,7 @@ export default function FormElements() {
         title="React.js Form Elements Dashboard | TailAdmin - React.js Admin Dashboard Template"
         description="This is React.js Form Elements  Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="From Elements" />
+      <PageBreadcrumb pageTitle="Form Elements" />
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
           <DefaultInputs />
