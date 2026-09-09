13c94da43c24a355c7e61b2461ab66d1c2820434
tusar-pimjo <tusar.pimjo@gmail.com>
2026-09-09 16:04:34 +0600

    chore: udpate packages and configure vite including other dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   plugins: [
     react(),
     svgr({
@@ -15,4 +20,18 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rolldownOptions: {
+      onwarn(warning, warn) {
+        // Skip eval warnings from react-jvectormap
+        if (
+          warning.code === "EVAL" &&
+          warning.id?.includes("@react-jvectormap")
+        ) {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 });
